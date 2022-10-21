0e4434f84d5cea83c90ede80a0cf49c3fe115d53
kudinov96 <ilya.kudinov96@yandex.ru>
2022-10-21 23:36:45 +0300

    add widgets to admin panel

---
+++ app/app/Widgets/UserDimmer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Widgets;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Widgets\BaseDimmer;
+
+class UserDimmer extends BaseDimmer
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-group',
+            'title'  => "Пользователи",
+            'text'   => "Редактирование пользователей",
+            'button' => [
+                'text' => "Перейти",
+                'link' => route('voyager.users.index'),
+            ],
+            'image' => voyager_asset('images/widget-backgrounds/01.jpg'),
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return Auth::user()->can('browse', Voyager::model('User'));
+    }
+}
