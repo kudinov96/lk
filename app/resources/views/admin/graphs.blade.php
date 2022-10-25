9f3256acbdb46aaa65f41c7904c74de3cc923aeb
kudinov96 <ilya.kudinov96@yandex.ru>
2022-10-25 12:19:22 +0300

    ban desired levels

--- app/resources/views/admin/graphs.blade.php
+++ app/resources/views/admin/graphs.blade.php
@@ -16,7 +16,7 @@
                     <div class="panel-body" style="padding:30px;">
                         @if($graphCategories)
                             <div class="dd">
-                                <ol class="dd-list">
+                                <ol class="dd-list dd-list_categories">
                                     @foreach($graphCategories as $graphCategory)
                                         <li class="dd-item" data-id="category-{{ $graphCategory->id }}" data-type="type1">
                                             <div class="pull-right item_actions">
@@ -29,7 +29,7 @@
                                             </div>
 
                                             @if($graphCategory->subcategories->count() > 0)
-                                                <ol class="dd-list">
+                                                <ol class="dd-list dd-list_subcategories">
                                                     @foreach($graphCategory->subcategories as $subcategory)
                                                         <li class="dd-item" data-id="subcategory-{{ $subcategory->id }}" data-type="type2">
                                                             <div class="pull-right item_actions">
@@ -42,7 +42,7 @@
                                                             </div>
 
                                                             @if($subcategory->tools->count() > 0)
-                                                                <ol class="dd-list">
+                                                                <ol class="dd-list dd-list_tools">
                                                                     @foreach($subcategory->tools as $tool)
                                                                         <li class="dd-item" data-id="tool-{{ $tool->id }}" data-type="type3">
                                                                             <div class="pull-right item_actions">
@@ -83,19 +83,26 @@
                 expandBtnHTML: '',
                 collapseBtnHTML: '',
                 maxDepth: 3,
-                onDragStart: function (l, e) {
-                    var type = $(e).data('type');
+                beforeDragStop: function (l, e, p) {
+                    let type = $(e).data('type');
 
                     switch (type) {
                         case 'type1':
-
+                            if ($(p).hasClass('dd-list_subcategories') || $(p).hasClass('dd-list_tools')) {
+                                return false;
+                            }
                             break;
-                        case 'type2':
 
+                        case 'type2':
+                            if ($(p).hasClass('dd-list_categories') || $(p).hasClass('dd-list_subcategories') || $(p).hasClass('dd-list_tools')) {
+                                return false;
+                            }
                             break;
 
                         case 'type3':
-
+                            if ($(p).hasClass('dd-list_categories') || $(p).hasClass('dd-list_tools')) {
+                                return false;
+                            }
                             break;
                         default:
                             console.error("Invalid type");
