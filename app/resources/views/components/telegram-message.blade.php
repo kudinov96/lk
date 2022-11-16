b6bb38086521ec23eeb7a616ea6b10dcc6679aaa
kudinov96 <ilya.kudinov96@yandex.ru>
2022-11-16 11:21:00 +0300

    fix

--- app/resources/views/components/telegram-message.blade.php
+++ app/resources/views/components/telegram-message.blade.php
@@ -1,6 +1,6 @@
 @if($message->from === \App\Enums\TelegramMessageFrom::BOT)
-    <p><span class="telegram-chat__bot">BOT ({{ $message->created_at->format("H:i d.m.Y") }}):</span> {{ strip_tags($message->text) }}</p>
+    <p class="telegram-chat__message" data-id="{{ $message->id }}"><span class="telegram-chat__bot">BOT ({{ $message->created_at->format("H:i d.m.Y") }}):</span> {{ strip_tags($message->text) }}</p>
 @endif
 @if($message->from === \App\Enums\TelegramMessageFrom::USER)
-    <p><span class="telegram-chat__user">{{ $user->name }} ({{ $message->created_at->format("H:i d.m.Y") }}):</span> {{ strip_tags($message->text) }}</p>
+    <p class="telegram-chat__message" data-id="{{ $message->id }}"><span class="telegram-chat__user">{{ $user->name }} ({{ $message->created_at->format("H:i d.m.Y") }}):</span> {{ strip_tags($message->text) }}</p>
 @endif
